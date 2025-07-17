c897f98277a2888a96f4e282e963ff3358d6146e
sakura2813 <i.wo.u.813@gmail.com>
2025-07-17 17:42:02 +0900

    Userモデルにカラムを追加し、バリデーションを設定

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # ニックネームと誕生日
+  validates :nickname, presence: true
+  validates :birth_date, presence: true
+
+  # 名前：全角ひらがな・カタカナ・漢字
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+
+  # 名前（カナ）：全角カタカナ
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ンー]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ンー]+\z/ }
+
+  # パスワード：英数字混合（Deviseのvalidatableがあるのでカスタムする場合のみ）
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/ }
+end
