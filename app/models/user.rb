4bd861544d6d4def5bbbb10b6b659ba736be6c20
sakura2813 <i.wo.u.813@gmail.com>
2025-07-18 14:26:12 +0900

    正規表現の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,12 @@ class User < ApplicationRecord
   validates :birth_date, presence: true
 
   # 名前：全角ひらがな・カタカナ・漢字
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
 
   # 名前（カナ）：全角カタカナ
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ンー]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ンー]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
 
   # パスワード：英数字混合（Deviseのvalidatableがあるのでカスタムする場合のみ）
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/ }
