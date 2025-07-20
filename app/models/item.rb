2b8559d15eafa710934c766d02cb13ed32534972
sakura2813 <i.wo.u.813@gmail.com>
2025-07-20 13:36:52 +0900

    ShippingDayモデル（ActiveHash）を作成しItemと関連付け

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,5 @@ class Item < ApplicationRecord
   belongs_to_active_hash :condition
   belongs_to_active_hash :shipping_fee
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_day
 end
