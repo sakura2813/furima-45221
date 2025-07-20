210828c5bf39586fc950571ad5c40e8f45fd45a2
sakura2813 <i.wo.u.813@gmail.com>
2025-07-20 13:34:47 +0900

    Prefectureモデル（ActiveHash）を作成しItemと関連付け

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :prefecture
 end
