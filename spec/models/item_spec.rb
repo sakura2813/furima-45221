d47ae03b2e95cbabcf10cc307ebae3e1edd0f73d
sakura2813 <i.wo.u.813@gmail.com>
2025-07-22 22:40:13 +0900

    ユーザーが紐づいていない場合とpriceが半角整数以外では保存できないことをテストに追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -107,5 +107,17 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
     end
+
+    it 'priceが半角整数以外では保存できない' do
+      @item.price = 'abc'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price is not a number')
+    end
+
+    it 'ユーザーが紐づいていないと保存できない' do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include('User must exist')
+    end
   end
 end
