041aad2f78beff389e059e46acd8c96dda086b42
sakura2813 <i.wo.u.813@gmail.com>
2025-07-22 17:28:50 +0900

    販売手数料と利益をカンマ区切りで表示するように修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -9,7 +9,7 @@ window.addEventListener('turbo:load', () => {
     const tax = Math.floor(price * 0.1); // 販売手数料：10%
     const gain = price - tax;            // 利益
 
-    taxPrice.innerText = tax;
-    profit.innerText = gain;
+    taxPrice.innerText = tax.toLocaleString();
+    profit.innerText = gain.toLocaleString();
   });
 });
