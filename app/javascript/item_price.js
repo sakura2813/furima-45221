c5ae85964fb08dabafd62322c55cd5afeda70f52
sakura2813 <i.wo.u.813@gmail.com>
2025-07-22 17:18:19 +0900

    JavaScriptで販売手数料と利益の自動表示を実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,15 @@
+window.addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById('item-price');
+  const taxPrice = document.getElementById('add-tax-price');
+  const profit = document.getElementById('profit');
+
+  priceInput.addEventListener('input', () => {
+    const price = parseInt(priceInput.value);
+
+    const tax = Math.floor(price * 0.1); // 販売手数料：10%
+    const gain = price - tax;            // 利益
+
+    taxPrice.innerText = tax;
+    profit.innerText = gain;
+  });
+});
