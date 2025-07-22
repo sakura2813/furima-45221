b9e93a102c7f1df3414435f83f6a78ac62299d42
sakura2813 <i.wo.u.813@gmail.com>
2025-07-22 19:37:00 +0900

    Itemモデルのテストデータと画像ファイルの設定を追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,21 @@
 FactoryBot.define do
   factory :item do
-    
+    name              { '商品名' }
+    description       { '商品の説明' }
+    category_id       { 2 }
+    condition_id      { 2 }
+    shipping_fee_id   { 2 }
+    prefecture_id     { 2 }
+    shipping_day_id   { 2 }
+    price             { 1000 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(
+        io: File.open(Rails.root.join('public/images/test_image.png')),
+        filename: 'test_image.png',
+        content_type: 'image/png'
+      )
+    end
   end
 end
