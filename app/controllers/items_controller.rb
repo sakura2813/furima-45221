46f02a32d130f758e33cd6c11c888269ae82cafe
sakura2813 <i.wo.u.813@gmail.com>
2025-07-24 20:50:45 +0900

    他のユーザーによる商品編集ページへのアクセスを制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :redirect_unless_owner, only: [:edit, :update]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -18,15 +20,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path # 更新に成功したらトップページへリダイレクト
     else
@@ -42,5 +41,13 @@ class ItemsController < ApplicationController
       :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id,
       :image
     ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def redirect_unless_owner
+    redirect_to root_path unless current_user == @item.user
   end
 end
