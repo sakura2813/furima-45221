cf7f98de62f1db76df2a90baae07d18f926dab1b
sakura2813 <i.wo.u.813@gmail.com>
2025-07-26 15:06:23 +0900

    購入情報入力用のバリデーションをPurchaseFormに実装

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -2,4 +2,15 @@ class PurchaseForm
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :street_addresses, :building,
                 :phone_number, :user_id, :item_id, :token
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city
+    validates :street_address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :token
+    validates :user_id
+    validates :item_id
+  end
 end
