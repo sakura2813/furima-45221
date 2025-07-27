81f43467408ec3afe3f9417e03556863f656f9b9
sakura2813 <i.wo.u.813@gmail.com>
2025-07-27 13:15:16 +0900

    購入機能のコントローラ実装（決済処理前まで）

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,34 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, expect: :index
+  before_action :set_item, only: [:new, :create]
+
+  def index
+  end
+
+  def new
+    @purchase_form = PurchaseForm.new
+  end
+
+  def create
+    @purchase_form = PurchaseForm.new(purchase_params)
+    if @purchase_form.valid?
+      @purchase_form.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def purchase_params
+    params.require(:purchase_form).permit(
+      :postal_code, :prefecture_id, :city, :street_address, :building,
+      :phone_number, :token
+    ).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+end
